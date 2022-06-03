8167899f20185c9beb1ab0b519dd6039cc377717
ren <autoeyy@gmail.com>
2022-06-03 19:05:42 +0900

    a

---
+++ map.dart
@@ -0,0 +1,24 @@
+// map
+// keyとvalueを紐づけて格納するオブジェクト(javascriptでいう所のオブジェクト)
+
+// 変数宣言
+// 型指定は左にkeyとなるもの(大体Stringな気がするけど)、右側にvalueの型を指定する
+Map<String, String> frameworksLang = {
+  "flutter": "Dart",
+  "rails": "Ruby",
+  "React": "JavaScript",
+};
+
+// 値を取り出す
+final flutterLang = frameworksLang["flutter"]; // Dartと出力される
+
+// 値を追加する
+void addFrameworksLang() {
+  frameworksLang["Nuxt"] = "vue";
+}
+
+main() {
+  print(flutterLang);
+  addFrameworksLang();
+  print(frameworksLang);
+}
