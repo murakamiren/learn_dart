c522098524771b380aa22b6674271ce75ede44a9
renmurakami <autoeyy@gmail.com>
2022-06-03 10:03:29 +0900

    json decode, factory

--- json.dart
+++ json.dart
@@ -1,3 +1,26 @@
+import 'dart:convert';
+import 'dart:ffi';
+
+const jsonData = '{ "name": "test太郎", "age": 12 }';
+
+class User {
+  final String name;
+  final int age;
+
+  User({
+    required this.name,
+    required this.age,
+  });
+
+  factory User.fromJson(Map<String, dynamic> data) {
+    final name = data["name"] as String;
+    final age = data["age"] as int;
+    return User(name: name, age: age);
+  }
+}
+
 main() {
-  print("aaa");
+  final parseJson = jsonDecode(jsonData);
+  final userData = User.fromJson(parseJson);
+  print(userData.age);
 }
